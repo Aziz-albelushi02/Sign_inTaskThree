a2c99767938d8447ceced46826ea0b42659c6694
Aziz-albelushi02 <ksa.xo1999@gmail.com>
2022-06-20 21:25:57 +0300

    init

---
+++ lib/pages/signed_in.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SignedIn extends StatelessWidget {
+  // Step 5
+  final String username;
+  const SignedIn({
+    Key? key,
+    required this.username,
+  }) : super(key: key);
+  // Step 6
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign in"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            // Step 9
+            Text("Welcome $username"),
+            Icon(
+              Icons.check_circle,
+              color: Colors.green,
+              size: 140,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
